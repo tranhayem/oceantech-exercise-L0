34ed54022980dba25c7b159b4230ada499b52e73
thy <mailcuathy@gmail.com>
2024-08-24 10:13:04 +0700

    done L0-Javascript Ex1

---
+++ L0-JavaScript/ex/Ex1.js
@@ -0,0 +1,77 @@
+export const listData = [
+  {
+    id: 1,
+    name: "Nike Air Force 1 '07",
+    price: 115,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png",
+    quantity: 0,
+  },
+  {
+    id: 2,
+    name: "Nike Dunk Low",
+    price: 90,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/56ff13a0-d2e6-49ed-bfd2-43dc2ba0922b/NIKE+DUNK+LOW+%28GS%29.png",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: "Nike Dunk Low Retro",
+    price: 115,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/35848a6a-4180-43b5-86a5-1caa7bf0c11b/NIKE+DUNK+LOW+RETRO.png",
+    quantity: 2,
+  },
+  {
+    id: 4,
+    name: "Air Jordan 6 Retro",
+    price: 200,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c4680930-5ef5-4e83-8d17-9c02fd25b841/AIR+JORDAN+6+RETRO.png",
+    quantity: 4,
+  },
+  {
+    id: 5,
+    name: "Air Jordan 1 Mid",
+    price: 82,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/61012d23-cbb9-42d6-871d-bc634201f0aa/AIR+JORDAN+1+MID.png",
+    quantity: 8,
+  },
+  {
+    id: 6,
+    name: "Nike Everyday Cushioned",
+    price: 24,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/rn4baqg7obokknxe2tab/U+NK+EVERYDAY+CUSH+CRW+6PR+132.png",
+    quantity: 128,
+  },
+  {
+    id: 7,
+    name: "Nike Dunk Low",
+    price: 115,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c09cf8bc-4d6c-4564-86d6-cc172deab92e/W+NIKE+DUNK+LOW.png",
+    quantity: 8,
+  },
+  {
+    id: 8,
+    name: "Nike Air Force 1 LE",
+    price: 90,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/38cad9d3-6574-406c-b015-23ca7f65533a/AIR+FORCE+1+LE+%28GS%29.png",
+    quantity: 4,
+  },
+  {
+    id: 9,
+    name: "Nike Air Max Dn",
+    price: 170,
+    imageUrl:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e61080a2-fbe3-4bd9-917b-673bb10f6c73/AIR+MAX+DN+OLY.png",
+    quantity: 2,
+  },
+];
+
+export const keyLocalStorageListSP = "DANHSACHSP";
+export const keyLocalStorageItemCart = "DANHSACHITEMCART";
