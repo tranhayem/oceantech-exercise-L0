5aa7209c496cf58dcfe30f0dfa56608e0ca2e952
thy <mailcuathy@gmail.com>
2024-08-29 10:33:14 +0700

    update L0-Javascript Ex3

--- L0-JavaScript/ex/Ex3.js
+++ L0-JavaScript/ex/Ex3.js
@@ -63,29 +63,27 @@ export const renderProducts = () => {
   const cartItems =
     JSON.parse(localStorage.getItem(keyLocalStorageItemCart)) || [];
 
-  const productElements = products.map((product) => {
-    if (product.quantity === 0) {
-      return;
-    }
-
-    return `<div class="col-sm-6 col-md-4 col-lg-3 mb-4">
-      <div class="card">
-        <div class="card-img">
-          <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
-          <button class="button-add " data-id="${product.id}">
-            <i class="fa-solid fa-cart-plus"></i>
-          </button>
-        </div>
-        <div class="card-body">
-          <p class="card-title text-center fs-6 fw-bold">${product.name}</p>
-          <div class="d-flex justify-content-between">
-            <p class="card-text m-0">$${product.price}</p>
-            <p class="card-text m-0">Quantity: ${product.quantity}</p>
-          </div>
-        </div>
-      </div>
-    </div>`;
-  });
+  const productElements = products
+    .filter((product) => product.quantity > 0)
+    .map((product) => {
+      return `<div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="card">
+                  <div class="card-img">
+                    <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+                    <button class="button-add " data-id="${product.id}">
+                      <i class="fa-solid fa-cart-plus"></i>
+                    </button>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-title text-center fs-6 fw-bold">${product.name}</p>
+                    <div class="d-flex justify-content-between">
+                      <p class="card-text m-0">$${product.price}</p>
+                      <p class="card-text m-0">Quantity: ${product.quantity}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+    });
 
   productListElement.innerHTML = productElements.join("");
 
