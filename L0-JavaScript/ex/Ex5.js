6aa23fe11c732d0d6748a5ed183f62597cb73640
thy <mailcuathy@gmail.com>
2024-08-29 17:07:39 +0700

    update L0-Javascript Ex5

--- L0-JavaScript/ex/Ex5.js
+++ L0-JavaScript/ex/Ex5.js
@@ -1,12 +1,20 @@
-Array.prototype.calculateTotal = function (key) {
-  return this.reduce((total, item) => total + (item[key] || 0), 0);
-};
+import { getProductById } from "./common.js";
 
-const cart = JSON.parse(localStorage.getItem(keyLocalStorageItemCart)) || [];
-const totalQuantity = cart.calculateTotal("soLuong");
-const totalPrice = cart.reduce((total, item) => {
-  const product = listData.find((p) => p.id === item.idSP);
-  return total + (product ? product.price * item.soLuong : 0);
-}, 0);
+export function calculateTotals(cartItems) {
+  let totalQuantity = 0;
+  let totalPrice = 0.0;
 
-console.log(`Total Quantity: ${totalQuantity}, Total Price: ${totalPrice}`);
+  if (cartItems.length > 0) {
+    cartItems.forEach((item) => {
+      totalQuantity += item.soLuong;
+      const product = getProductById(item.idSP);
+      totalPrice += product.price * item.soLuong;
+    });
+  }
+
+  const totalsMap = new Map();
+  totalsMap.set("totalQuantity", totalQuantity);
+  totalsMap.set("totalPrice", totalPrice.toFixed(2));
+
+  return totalsMap;
+}
