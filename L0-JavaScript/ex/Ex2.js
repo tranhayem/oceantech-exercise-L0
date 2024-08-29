217ec44690171469a0b6757844e6d43a41c61967
thy <mailcuathy@gmail.com>
2024-08-29 10:01:14 +0700

    update L0-Javascript Ex2

--- L0-JavaScript/ex/Ex2.js
+++ L0-JavaScript/ex/Ex2.js
@@ -3,8 +3,15 @@ import { listData, keyLocalStorageListSP } from "./Ex1.js";
 export const saveDataToLocalStorage = () => {
   if (!Array.isArray(listData)) {
     console.error("listData is not an array");
+
+    return;
+  }
+
+  if (listData.length === 0) {
     return;
   }
 
   localStorage.setItem(keyLocalStorageListSP, JSON.stringify(listData));
 };
+
+saveDataToLocalStorage();
