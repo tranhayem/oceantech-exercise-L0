5bb2a39cc581de13f0966c07b6ed7d6737d79650
thy <mailcuathy@gmail.com>
2024-08-31 17:57:29 +0700

    update L0-Javascript Ex12

--- L0-JavaScript/ex/Ex12.js
+++ L0-JavaScript/ex/Ex12.js
@@ -1,8 +1,17 @@
-export const saveToLocalStorage = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data));
+export const saveToLocalStorage = (key, value = []) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error("Failed to save data to localStorage:", error);
+  }
 };
 
-export const getFromLocalStorage = (key) => {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
+export const getFromLocalStorage = (key, defaultValue = []) => {
+  try {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : defaultValue;
+  } catch (error) {
+    console.error("Failed to get data from localStorage:", error);
+    return defaultValue;
+  }
 };
