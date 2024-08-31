212ac40c6a950d70a5e2b01e67380a6b0dc043fc
thy <mailcuathy@gmail.com>
2024-08-31 16:10:38 +0700

    update L0-Javascript Ex9

--- L0-JavaScript/ex/Ex9.js
+++ L0-JavaScript/ex/Ex9.js
@@ -1,27 +1,60 @@
-export const getDistrictsByProvinceID = async (provinceID) => {
-  try {
-    const response = await fetch(`https://provinces.open-api.vn/api/d/`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch districts");
-    }
-    const data = await response.json();
-    return data.filter((district) => district.province_code === provinceID);
-  } catch (error) {
-    console.error(error);
-    return [];
+import { getProvinceData, loadDistrictData, loadWardData } from "./Ex8.js";
+
+const provinceSelect = document.getElementById("province");
+const districtSelect = document.getElementById("district");
+const wardSelect = document.getElementById("ward");
+
+const initProvinces = async () => {
+  const provinces = await getProvinceData();
+  if (provinces) {
+    provinces.forEach((province) => {
+      const option = document.createElement("option");
+      option.value = province.code;
+      option.textContent = province.name;
+      provinceSelect.appendChild(option);
+    });
   }
 };
 
-export const getWardsByDistrictsID = async (districtID) => {
-  try {
-    const response = await fetch(`https://provinces.open-api.vn/api/w/`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch wards");
-    }
-    const data = await response.json();
-    return data.filter((ward) => ward.district_code === districtID);
-  } catch (error) {
-    console.error(error);
-    return [];
+const loadDistricts = async (provinceId) => {
+  const districts = await loadDistrictData(provinceId);
+  if (Array.isArray(districts)) {
+    districtSelect.innerHTML =
+      '<option selected disabled value="">--Chọn Huyện/Quận--</option>';
+    districts.forEach((district) => {
+      const option = document.createElement("option");
+      option.value = district.code;
+      option.textContent = district.name;
+      districtSelect.appendChild(option);
+    });
+    districtSelect.disabled = false;
   }
 };
+
+const loadWards = async (districtId) => {
+  const wards = await loadWardData(districtId);
+  if (Array.isArray(wards)) {
+    wardSelect.innerHTML =
+      '<option selected disabled value="">--Chọn Phường/Xã--</option>';
+    wards.forEach((ward) => {
+      const option = document.createElement("option");
+      option.value = ward.code;
+      option.textContent = ward.name;
+      wardSelect.appendChild(option);
+    });
+    wardSelect.disabled = false;
+  }
+};
+
+provinceSelect.addEventListener("change", (event) => {
+  const provinceId = event.target.value;
+  loadDistricts(provinceId);
+  wardSelect.disabled = true;
+});
+
+districtSelect.addEventListener("change", (event) => {
+  const districtId = event.target.value;
+  loadWards(districtId);
+});
+
+initProvinces();
