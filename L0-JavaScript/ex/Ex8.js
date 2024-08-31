83fe5e41416d1e63b76c6eb0f64782d9d0623ebd
thy <mailcuathy@gmail.com>
2024-08-31 16:10:31 +0700

    update L0-Javascript Ex8

--- L0-JavaScript/ex/Ex8.js
+++ L0-JavaScript/ex/Ex8.js
@@ -1,13 +1,28 @@
-export const fetchProvinces = async () => {
+const baseUrl = "https://provinces.open-api.vn/api";
+
+const fetchData = async (url) => {
   try {
-    const response = await fetch("https://provinces.open-api.vn/api/");
-    if (!response.ok) {
-      throw new Error("Failed to fetch provinces");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return [];
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+    return await res.json();
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    return null;
   }
 };
+
+export const getProvinceData = () => fetchData(`${baseUrl}/p`);
+
+export const getDistrictsByProvinceID = (provinceId) =>
+  fetchData(`${baseUrl}/p/${provinceId}?depth=2`).then(
+    (data) => data?.districts || []
+  );
+
+export const getWardsByDistrictsID = (districtId) =>
+  fetchData(`${baseUrl}/d/${districtId}?depth=2`).then(
+    (data) => data?.wards || []
+  );
+
+export const loadDistrictData = getDistrictsByProvinceID;
+
+export const loadWardData = getWardsByDistrictsID;
